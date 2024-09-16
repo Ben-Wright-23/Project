28cc53ad0fc1d816e85911202432fc2fe5b35756
Ben Wright <7855@balcarras.gloucs.sch.uk>
2024-09-16 14:32:46 +0000

    built basic login and signup

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from flask import Flask
+#Imports#############################################
+from flask import Flask, render_template
+
+
+#####################################################
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "THISISABADKEY"
 
+#Routing#############################################
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("SignUp.html")
 
 
 
-
-
-
-
-####
+######################################################
 app.run(debug = True)
