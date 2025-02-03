4ebc7aa647cbc99890ee3523725734ce03b95074
Ben-Wright-4 <benwright2302@gmail.com>
2025-02-03 20:00:09 +0000

    .

--- routes/tournamentCreation.py
+++ routes/tournamentCreation.py
@@ -51,34 +51,50 @@ def tournamentCreation():
 
 
 @teamsInputPageBlueprint.route("/teamsInputPage")
+#creates the route for the teamsInputPage blueprint, allowing it to be accessed easily
 def teamsInputPage():
+    #defines function to load teamsInputPage
     Error = session.get("teamInputError") if session.get("teamInputError") else ""
     DeletionError = session.get("teamDeletionError") if session.get("teamDeletionError") else ""
+    #Produces the errors with the team inputting or deleting if there is one
+    #if no error for either, no error is passed for that error to the interface
     return render_template("teamsInput.html", error = Error, error2 = DeletionError)
-
+    #loads the creationForm.html page, with the error messages displayed when it is reloaded
 
 @teamsInputBlueprint.route("/teamsInput", methods = ["POST"])
+#creates the route for the teamsInput blueprint, allowing it to be accessed easily. Post method allows it to send data to the server
 def teamsInput():
+    #defines teamsInput function  
     if len(teams)<int(numTeams):
+        #creates condition to check there is less teams in the list/tournament than the user previously specified was how many teams their tournament would contain
         newTeamName = request.form["teamNames"]
+        #takes the entered team name sent from the teamsInput page(the client) to the server, using the form input with id "teamNames".
         # for i in teams:
         #     if newTeamName==i:
         #         session["teamInputError"] = "Teams must have unique name"
         #         session["Teams"] = session["Teams"]
-        #         return teamsInputPage()
+        #         return redirect("/teamsInputPage")
         if newTeamName != "":
+            #Checks the user has entered a value for the team name
             teams.append(newTeamName)
+            #adds the new team to the list of teams if there is a team name entered
             session["Teams"] = teams
+            #update the session for teams to be the new list of teams so it can be displayed to the user in the teamsInput html page
             session["teamInputError"] = ""
-            return teamsInputPage()
+            #The team entered is valid so there is no error with the team input
+            return redirect("/teamsInputPage")
+            #Reloads the teamInput page with the new team added to the list displayed to the user
         else:
             session["teamInputError"] = "Team must have a name"
-            return teamsInputPage()
+            #If no name for the team entered, output an error message stating this
+            return redirect("/teamsInputPage")
+            #Reloads the teamInput page with the error displayed to the user
     else:
         session["teamInputError"] = "Your amount of teams has been reached"
-        return teamsInputPage()
-    
-    
+        #If there are as many teams in the list as that the user has said their tournament should contain, the new team is not entered and this error is displayed
+        return redirect("/teamsInputPage")
+        #Reloads the teamInput page with the error displayed to the user
+
 
 @teamDeletionBlueprint.route("/teamDeletion", methods = ["POST"])
 def teamDeletion():
@@ -93,13 +109,13 @@ def teamDeletion():
         else:
             session["teamDeletionError"] = "Team not in tournament"
     
-    return teamsInputPage()
+    return redirect("/teamsInputPage")
 
 @clearTeamsBlueprint.route("/clearTeams", methods = ["POST"])
 def clearTeams():
     teams.clear()
     session["Teams"] = teams
-    return teamsInputPage()
+    return redirect("/teamsInputPage")
     
     
 @bracketViewBlueprint.route("/bracketView")
