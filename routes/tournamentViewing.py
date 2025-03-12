d67f155b262a3b85df14184dfd90d81d8cd20096
Ben Wright <7855@balcarras.gloucs.sch.uk>
2025-03-12 19:58:02 +0000

    .

--- routes/tournamentViewing.py
+++ routes/tournamentViewing.py
@@ -9,13 +9,22 @@ viewTournamentBlueprint = Blueprint("viewTournament",__name__)
 def viewTournament():
     #defines viewTournament function for the viewTournament blueprint
     db = DatabaseHandler("appData.db")
+    #creates a link to the database, where appData.db is the database storing the enities
     enteredViewCode = request.form["enteredViewCode"]
+    #takes the entered view code from the view code input on the dashboard, using the form input with name "enteredViewCode".
     session["Viewing"] = True
+    #sets the viewing session to be true, indication the tournament is currently being viewed so access should be restricted
     if db.checkViewCodes(enteredViewCode) == None:
         session["viewCodeInputError"] = "The entered view code does not exist"
+        #if the view code is not present in the database, an error is returned to the user stating this
         return redirect("/dashboard")
+        #reloads the dashboard page with this error displayed
     else:
         session["viewCodeInputError"] = ""
+        #clears the view code input errros session as no error has occured with the view code input
         tournamentName = db.getTournamentName(enteredViewCode)
+        #retrieves the tournament name of the tournament that has had its view code entered
         session["Tournament"] = tournamentName
+        #sets the tournament session to be the tournament name, so the correct tournament dashboard can be displayed
         return redirect("/tournamentDashboard")
+        #loads the restricted tournament dashboard for the tournament that has had its view code entered
